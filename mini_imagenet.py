af4b6d68ada11c3af86fe716b774202b6e8b763a
Matthew Wallingford <matt@matthews-mbp.home>
2019-12-17 00:30:38 -0800

    changed mini_imagenet

--- mini_imagenet.py
+++ mini_imagenet.py
@@ -77,6 +77,18 @@ def splitImageNet(sup_ratio):
     df[~msk].to_csv(unsup_path)
     df[msk].to_csv(sup_path)
 
+def splitImageNet(sup_ratio):
+    setname = 'train'
+    csv_path = osp.join(ROOT_PATH, setname + '.csv')
+    unsup_path = osp.join(ROOT_PATH, 'unsup' + '.csv')
+    sup_path = osp.join(ROOT_PATH, 'sup' + '.csv')
+    df = pd.read_csv(csv_path)
+    msk = np.random.rand(len(df)) < 0.4
+    print('Number of supervised samples: {}'.format(sum(msk)))
+    print('Number of unsupervised samples: {}'.format(sum(~msk)))
+    df[~msk].to_csv(unsup_path)
+    df[msk].to_csv(sup_path)
+
 
 class SSMiniImageNet(Dataset):
     def __init__(self):
@@ -150,3 +162,42 @@ class SSMiniImageNet(Dataset):
         uimage = self.transform(Image.open(upath).convert('RGB'))
         return image, uimage
 
+
+class SplitMiniImageNet(Dataset):
+    def __init__(self, split):
+        setname = 'train'
+        lines = []
+        for i in range(split):
+            path = osp.join(ROOT_PATH, setname + str(i) + '.csv')
+            lines.append([x.strip() for x in open(path, 'r').readlines()][1:])
+
+        data = []
+        label = []
+        lb = -1
+        self.wnids = []
+        for line in lines:
+            for l in line:
+                name, wnid = l.split(',')
+                path = osp.join(ROOT_PATH, 'images', name)
+                if wnid not in self.wnids:
+                    lb += 1
+                    self.wnids.append(wnid)
+                data.append(path)
+                label.append(lb)
+
+        self.transform = transforms.Compose([
+            transforms.Resize(84),
+            transforms.CenterCrop(84),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225])
+        ])
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, i):
+        path, label = self.data[i], self.label[i]
+        print(label)
+        image = self.transform(Image.open(path).convert('RGB'))
+        return image, label
